1c31997303c7bbc8ff6bb4451e8963d03ad326c1
CleverOaf <107604919+CleverOaf@users.noreply.github.com>
2022-07-06 04:26:12 -0500

    Enable Walk Animations
    Walk animations now function properly for each animal.

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -24,16 +24,40 @@ public class Animal : MonoBehaviour
         }
     }
 
-    protected Animal m_Target;
+    protected bool atDestination = true;
+    protected Animator anim;
     protected NavMeshAgent m_Agent;
     protected float Speed = 3;
 
     protected void Awake()
     {
+        anim = GetComponent<Animator>();
+
         m_Agent = GetComponent<NavMeshAgent>();
         m_Agent.speed = Speed;
         m_Agent.acceleration = 999;
         m_Agent.angularSpeed = 999;
+    }
+
+    protected void Update()
+    {
+        if (m_Agent.transform.position == m_Agent.destination)
+        {
+            atDestination = true;
+        }
+        else
+        {
+            atDestination = false;
+        }
+
+        if (m_Agent != null && !atDestination && anim != null)
+        {
+            anim.SetInteger("Walk", 1);
+        }
+        else
+        {
+            anim.SetInteger("Walk", 0);
+        }
     }
 
     public virtual void Vocalize()
@@ -44,7 +68,6 @@ public class Animal : MonoBehaviour
     public virtual void GoTo(Vector3 position)
     {
         //we don't have a target anymore if we order to go to a random point.
-        m_Target = null;
         m_Agent.SetDestination(position);
         m_Agent.isStopped = false;
     }
